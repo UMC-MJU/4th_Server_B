2a7e2722940e5eae1e688b1c6c388a320c00a407
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-05-08 13:19:50 +0900

    FEAT: 5주차 미션 반복문 2번 A+B - 3 코드 업로드

---
+++ 데이빗_최현준/5주차 미션/반복문/A+B - 3.java
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/10950 반복문 2번 A+B - 3 문제 코드
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int number = Integer.parseInt(br.readLine());
+        for (int i = 0; i < number; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a + b + "\n");
+        }
+        bw.close();
+        br.close();
+    }
+}
