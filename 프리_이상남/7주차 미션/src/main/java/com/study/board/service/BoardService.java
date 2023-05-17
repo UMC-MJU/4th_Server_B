b14967e27b3153a6f0eacfef03054100e54c6237
이상남 <lsn5963@naver.com>
2023-05-18 00:22:50 +0900

    7주차미션

---
+++ 프리_이상남/7주차 미션/src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,52 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
+
+@Service
+public class BoardService {
+
+    @Autowired  //자동으로 주입해준다
+    private BoardRepository boardRepository;
+
+    //글작성
+    public void write(Board board, MultipartFile file) throws Exception{
+
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/" + fileName);
+
+        boardRepository.save(board);
+    }
+
+    //게시글 리스트 처리
+    public Page<Board> boardList(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
+
+    //특정 게시글 불러오기
+    public Board boardView(Integer id) {
+        return boardRepository.findById(id).get();
+    }
+
+    public void BoardDelete(Integer id) {
+        boardRepository.deleteById(id);
+    }
+}
