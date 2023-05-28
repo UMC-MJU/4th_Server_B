b6f126a6ca67868c3984fd18484afa885bb09ee4
choeun7 <short0720@gmail.com>
2023-05-28 21:47:42 +0900

    [FEAT]: 9주차미션 기본세팅

---
+++ 제리_신초은/week9/api-develop/src/main/java/umc/apidevelop/ApiDevelopApplication.java
@@ -0,0 +1,13 @@
+package umc.apidevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDevelopApplication.class, args);
+	}
+
+}
---
+++ 제리_신초은/week9/api-develop/src/test/java/umc/apidevelop/ApiDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.apidevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
